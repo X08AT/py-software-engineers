f0aef0c281e98b350fa0c7156484550f9f1a2734
Artem Turchyn <Artem.at131@gmail.com>
2026-02-28 21:50:04 +0200

    update

--- app/main.py
+++ app/main.py
@@ -41,10 +41,6 @@ class AndroidDeveloper(SoftwareEngineer):
 
 
 class FullStackDeveloper(FrontendDeveloper, BackendDeveloper):
-    def __init__(self, name: str) -> None:
-        super().__init__(name)
-        self.skills = ["JavaScript", "HTML", "CSS", "Python", "SQL", "Django"]
-
     def create_web_application(self) -> None:
         print(f"{self.name} started creating a web application...")
         self.create_powerful_api()
